c49c2e666fa0bdf8a2430892f43ab630a9458daf
roberthsu2003 <roberthsu2003@gmail.com>
2025-11-22 11:54:39 +0800

    modify

--- lesson3/lesson3_6.py
+++ lesson3/lesson3_6.py
@@ -26,3 +26,33 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+    from playwright.sync_api import sync_playwright
+from time import sleep
+
+def main():
+    with sync_playwright() as p:
+        print(f"p={type(p)}")
+        # 啟動瀏覽器
+        browser = p.webkit.launch(headless=False)
+        
+        print(f"browser:{type(browser)}")
+        # 開啟新分頁
+        page = browser.new_page()
+
+        print(f"page:{type(page)}")
+        
+        # 訪問網站
+        page.goto("https://www.google.com")
+        
+        # 取得標題
+        print(page.title())
+
+        sleep(20)
+        
+        # 關閉瀏覽器
+        browser.close()
+
+if __name__ == "__main__":
+    main()
+    
